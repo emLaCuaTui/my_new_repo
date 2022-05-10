5c317516d4e8dc736778d502ecf7968b5f49e49a
emlacuatui <trinhdinhbing@gmail.com>
2022-05-10 19:05:46 +0700

    h

---
+++ ChineseChef.py
@@ -0,0 +1,12 @@
+from Chef import Chef
+
+
+class ChineseChef(Chef):
+    def make_special_dish(self):
+        print("The chef makes tuna")
+        
+    def make_fried_rice(self):
+        print("The chef makes fried rice")
+        
+    def kill_people_with_knife():
+        print("This is a commit from 2:37")
